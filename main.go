5db7d262e02cd881a4297e982230270505e0fee5
Saadyyyy <laodesaady12345@gmail.com>
2023-12-03 16:28:19 +0700

    update

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ func main() {
 	// if err := godotenv.Load(); err != nil {
 	// 	log.Fatalf("Error loading .env file: %v", err)
 	// }
+
 	router := config.SetupRouter()
 	err := router.Start(":8080")
 	if err != nil {
